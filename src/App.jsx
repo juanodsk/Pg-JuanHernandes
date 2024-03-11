13ac1f67c69f0f250bcff22f1c0c50f65fbbc28c
Juan Manuel <juanph.dv@gmail.com>
2024-03-10 21:19:08 -0500

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Editard <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn morezzzz
       </p>
     </>
   )
