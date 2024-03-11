3faf2b9c00ecd87135d34c8141b7a9c3a31f8e57
ConnorA0 <144062573+JavierAnd0@users.noreply.github.com>
2024-03-10 21:16:29 -0500

    mi primera chamba

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Hola Mundo juan es gei
+        Hola Mundo juan es tremenda loca
       </p>
     </>
   )
